bbfae841ac0854ad8001b1756166453f307b31cf
yori3o <yori3o784512@gmail.com>
2026-01-17 15:35:48 +0300

    Delete common directory

--- common/src/main/java/com/yori3o/yo_hooks/YoHooksClientPlatformInit.java
+++
@@ -1,9 +0,0 @@
-package com.yori3o.yo_hooks;
-
-
-public class YoHooksClientPlatformInit {
-
-    public static void makeHooksItem() {
-        throw new RuntimeException("Platform-specific implementation missing");
-    }
-}
--- common/src/main/java/com/yori3o/yo_hooks/world/LootInjector.java
+++
@@ -1,8 +0,0 @@
-package com.yori3o.yo_hooks.world;
-
-public class LootInjector {
-
-    public static void register() {
-        throw new RuntimeException("Platform-specific implementation missing");
-    }
-}
