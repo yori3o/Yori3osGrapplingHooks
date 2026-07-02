c657a3c41e64317015750bd0b2c6782093280765
yori3o <yori3o784512@gmail.com>
2026-07-02 11:53:17 +0300

    custom description

--- src/main/java/com/yori3o/yo_hooks/common/config/VrConfig.java
+++ src/main/java/com/yori3o/yo_hooks/common/config/VrConfig.java
@@ -9,6 +9,7 @@ import dev.isxander.yacl3.config.v2.api.ConfigClassHandler;
 import dev.isxander.yacl3.config.v2.api.SerialEntry;
 import dev.isxander.yacl3.config.v2.api.autogen.AutoGen;
 import dev.isxander.yacl3.config.v2.api.autogen.Boolean;
+import dev.isxander.yacl3.config.v2.api.autogen.CustomDescription;
 import dev.isxander.yacl3.config.v2.api.serializer.GsonConfigSerializerBuilder;
 
 
@@ -26,5 +27,6 @@ public class VrConfig {
     @AutoGen(group = "vivecraft_compat", category = "client")
     @Boolean
     @SerialEntry
+    @CustomDescription("config.yo_hooks.a.desc")
     public boolean a = false;
 }
